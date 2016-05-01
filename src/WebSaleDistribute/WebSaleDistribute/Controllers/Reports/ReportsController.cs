5a685f12efc4fd94f5dd31906f87f640ce05f123
BehzadKhosravifar <behzadkh@hotmail.com>
2016-05-01 17:38:13 +0430

    add customersOrdersChart view

---
+++ src/WebSaleDistribute/WebSaleDistribute/Controllers/Reports/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using WebSaleDistribute.Core;
+using System.Data;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Options;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using System.Threading.Tasks;
+using WebSaleDistribute.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Globalization;
+
+namespace WebSaleDistribute.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        
+
+        // GET: Receipts
+        public ActionResult Sales()
+        {
+            ViewBag.Title = "گزارشات فروش";
+
+
+
+            return View();
+        }
+
+    }
+}
