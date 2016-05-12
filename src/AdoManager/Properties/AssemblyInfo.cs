440e880a15d05028485cc91cbfd1460f34b8f7f3
BehzadKhosravifar <behzadkh@hotmail.com>
2016-05-12 15:36:59 +0430

    change ado.net

--- src/AdoManager/Properties/AssemblyInfo.cs
+++ src/AdoManager/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AdoManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a C#.Net project's for manage database connection strings of your .Net applications.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Xomorod")]
 [assembly: AssemblyProduct("AdoManager")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016")]
+[assembly: AssemblyTrademark("Xomorod")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
--- src/WebSaleDistribute/WebSaleDistribute/App_Data/CallWebServiceFromSql_Script.sql
+++ src/WebSaleDistribute/WebSaleDistribute/App_Data/CallWebServiceFromSql_Script.sql
@@ -2,25 +2,24 @@
  * Call Sale Web Service Restful method to create QR Code and save on sql database
  ************************************************************/
 
-DECLARE @xmlOut VARCHAR(MAX),
-        @RequestText AS VARCHAR(8000),
-        @TargetSqlConn NVARCHAR(1000) =
-        'Data Source=localhost;Initial Catalog=TestDB;Password=123;Persist Security Info=True;User ID=sa;',
-        @Width VARCHAR(5) = 100,
-        @Height VARCHAR(5) = 100,
-        @RecordId VARCHAR(10) = 11,
-        @Content NVARCHAR(1000) = 'New GUID=' + CONVERT(NVARCHAR(100), NEWID()),
-        @url NVARCHAR(MAX) =
-        'http://localhost:33033/general/GenerateQRByStoring/';
-
-SET @url += @Width + '/' + @Height + '/' + @Content + '/' + @RecordId + '/' + @TargetSqlConn;
-
-EXEC spHTTPRequest 
-     @url,
-     'GET',
-     @RequestText,
-     '',
-     '',
-     @xmlOut OUT
+DECLARE @xmlOut VARCHAR(MAX) ,
+        @RequestText AS VARCHAR(8000) ,
+        @TargetSqlConn NVARCHAR(1000) = 'Data Source=' + @@SERVERNAME
+        + ';Initial Catalog=' + DB_NAME() + ';Integrated Security=true;' ,
+        @Width VARCHAR(5) = 100 ,
+        @Height VARCHAR(5) = 100 ,
+        @RecordId VARCHAR(10) = CONVERT(VARCHAR, @tkey) ,
+        @Content NVARCHAR(250) = CONVERT(NVARCHAR(50), NEWID()) ,
+        @ServerUrl VARCHAR(100) ,
+        @url NVARCHAR(3000) = 'general/GenerateQRByStoring';
+	
+    SELECT  @ServerUrl = [value]
+    FROM    settings s
+    WHERE   s.[key] = 'WebSaleDistributeUrl'
+	
+    SET @url = @ServerUrl + '/' + @url + '/' + @Width + '/' + @Height + '/'
+        + @Content + '/' + @RecordId + '/' + @TargetSqlConn;
+	
+    EXEC spHTTPRequest @url, 'GET', @RequestText, '', '', @xmlOut OUT
 
 SELECT @xmlOut 
