1aa05c773492e25e884ef4538aad87e2c3f1f601
BehzadKhosravifar <behzadkh@hotmail.com>
2016-05-31 13:06:03 +0430

    Add VersionUpdator.tt file to project

--- src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
+++ src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.2.*")]
-[assembly: AssemblyFileVersion("3.2.1.9")]
+[assembly: AssemblyVersion("3.3.24")]
+[assembly: AssemblyFileVersion("3.3.24")]
