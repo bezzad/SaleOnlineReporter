b4c19a1bf0b70831405d028e551ff0b71a8988d6
behzadkhosravifar <behzad.khosravifar@gmail.com>
2016-07-16 13:12:14 +0430

    fix report api for orders

--- src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
+++ src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.7.16")]
-[assembly: AssemblyFileVersion("3.7.16")]
+[assembly: AssemblyVersion("3.7.17")]
+[assembly: AssemblyFileVersion("3.7.17")]
