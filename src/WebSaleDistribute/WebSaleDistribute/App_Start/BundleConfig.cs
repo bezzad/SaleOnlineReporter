69fa735c8b2e5c52adb3b06e89f37b5001cff793
behzadkhosravifar <behzad.khosravifar@gmail.com>
2016-07-16 13:00:38 +0430

    fix publish address

--- src/WebSaleDistribute/WebSaleDistribute/App_Start/BundleConfig.cs
+++ src/WebSaleDistribute/WebSaleDistribute/App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace WebSaleDistribute
         {
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*", "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                                        "~/Scripts/jquery-{version}.js",
                                         "~/Scripts/jquery.unobtrusive-ajax.min.js",
                                         "~/Scripts/jquery-ui.min.js",
                                         "~/Scripts/jquery.ui.touch-punch.min.js",
