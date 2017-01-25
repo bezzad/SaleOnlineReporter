4d4863948c368db353a581a816c5c7831f4e514c
aghazadeh.a <ahmad.aghazadeh.a@gmail.com>
2017-01-25 10:36:55 +0330

    Add OrderPointViewModel

---
+++ src/WebSaleDistribute/WebSaleDistribute/Models/OrderPointViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSaleDistribute.Models
+{
+    
+    public class OrderPointViewModels
+    {
+        [Display(Name = "نام مشتری")]
+        public string PersonName { get; set; }
+
+        [Display(Name = "کد مشتری")]
+        public string PersonID { get; set; }
+
+
+        [Display(Name = "وضعیت")]
+        public int GPSStatus { get; set; }
+
+        [Display(Name = "درصد باطری")]
+        public int BatteryStatus { get; set; }
+
+
+        [Display(Name = "دقت")]
+        public int Accuracy { get; set; }
+
+        [Display(Name = "شماره درخواست")]
+        public int OrderNo { get; set; }
+
+
+        [Display(Name = "عرض جغرافیایی")]
+        public double Latitude { get; set; }
+
+        [Display(Name = "طول جغرافیایی")]
+        public double Longitude { get; set; }
+    }
+ 
+}
