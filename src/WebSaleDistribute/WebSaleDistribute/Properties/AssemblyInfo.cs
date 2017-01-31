42f8a04744f1963f42ce41f5848bbacd96057cef
Ahmad Aghazadeh <ahmad.aghazadeh.a@gmail.com>
2017-01-31 10:50:24 +0330

    Fix big problem

--- src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
+++ src/WebSaleDistribute/WebSaleDistribute/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.10.96")]
-[assembly: AssemblyFileVersion("3.10.96")]
+[assembly: AssemblyVersion("3.10.99")]
+[assembly: AssemblyFileVersion("3.10.99")]
