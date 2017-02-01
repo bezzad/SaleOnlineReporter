438662d5d123abd928f2bec8b2eed6f21a0ddd19
Ahmad Aghazadeh <ahmad.aghazadeh.a@gmail.com>
2017-02-01 14:33:22 +0330

    Add lose file

---
+++ src/WebSaleDistribute/WebSaleDistribute/Models/SginInWHCode.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSaleDistribute.Models
+{
+    public class SginInWHCode
+    {
+
+        [Display(Name = "ردیف")]
+        public string Row { get; set; }
+
+        [Display(Name = "شماره")]
+        public string WHCode { get; set; }
+
+        [Display(Name = "تاریخ")]
+        public string RegDate { get; set; }
+
+        [Display(Name = "کد محصول")]
+        public string ProductCode { get; set; }
+
+
+        [Display(Name = "نام محصول ")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "توضیحات")]
+        public string ProductDescription { get; set; }
+
+        [Display(Name = " تعداد")]
+        public int Qty { get; set; }
+        
+    }
+ 
+}
